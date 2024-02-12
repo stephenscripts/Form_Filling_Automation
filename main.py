59d201f1e2b896998377a394bfec739986a230aa
Nnamani StephenScripts <155476794+stephenscripts@users.noreply.github.com>
2024-02-12 17:03:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,19 +5,19 @@ import time
 web = webdriver.Chrome()
 web.get('https://appointment.bmeia.gv.at/')
 
-time.sleep(2)
+time.sleep(1)
 
 countries = web.find_element(By.XPATH, '//*[@id="Office"]/option[2]')
 countries.click()
 next_Button = web.find_element(By.XPATH, '//*[@id="main"]/form/table[2]/tbody/tr[2]/td[2]/input')
 next_Button.click()
-time.sleep(2)
+time.sleep(1)
 
 verification_legalization = web.find_element(By.XPATH, '//*[@id="CalendarId"]/option[3]')
 verification_legalization.click()
 next_Button = web.find_element(By.XPATH, '//*[@id="main"]/form/table[2]/tbody/tr[3]/td[2]/input[2]')
 next_Button.click()
-time.sleep(2)
+time.sleep(1)
 
 number_of_persons = web.find_element(By.XPATH, '//*[@id="PersonCount"]/option[1]')
 number_of_persons.click()
@@ -26,4 +26,11 @@ next_Button.click()
 
 next_Button = web.find_element(By.XPATH, '//*[@id="main"]/form/input[6]')
 next_Button.click()
+time.sleep(1)
+
+roll_number = 3
+dates = web.find_element('//*[@id="main"]/form/table[1]/tbody/tr/td[1]')
+# print(str(dates))
+
 time.sleep(2)
+
